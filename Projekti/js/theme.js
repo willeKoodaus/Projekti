fc27df586e128ce443b311d7524dd44ed9c3487d
Riku Nunes <toni.nunes@metropolia.fi>
2022-03-10 20:56:04 +0200

    - added new script to handle local storage and switching of theme when moving between html files. (using Storage and Window API)

--- Projekti/js/theme.js
+++ Projekti/js/theme.js
@@ -13,38 +13,47 @@ function changeTheme() {
   switch (this.name) {
     case 'mainTheme1':
       link.setAttribute('href', "css/main.css");
+      sessionStorage.setItem('theme', 'theme1');
       break;
 
     case 'mainTheme2':
       link.setAttribute('href', "css/main2.css");
+      sessionStorage.setItem('theme', 'theme2');
       break;
 
     case 'mainTheme3':
       link.setAttribute('href', "css/main3.css");
+      sessionStorage.setItem('theme', 'theme3');
       break;
 
     case 'infoTheme1':
       link.setAttribute('href', "css/info.css");
+      sessionStorage.setItem('theme', 'theme1');
       break;
 
     case 'infoTheme2':
       link.setAttribute('href', "css/info2.css");
+      sessionStorage.setItem('theme', 'theme2');
       break;
 
     case 'infoTheme3':
       link.setAttribute('href', "css/info3.css");
+      sessionStorage.setItem('theme', 'theme3');
       break;
 
     case 'authorsTheme1':
       link.setAttribute('href', "css/authors.css");
+      sessionStorage.setItem('theme', 'theme1');
       break;
 
     case 'authorsTheme2':
       link.setAttribute('href', "css/authors2.css");
+      sessionStorage.setItem('theme', 'theme2');
       break;
 
     case 'authorsTheme3':
       link.setAttribute('href', "css/authors3.css");
+      sessionStorage.setItem('theme', 'theme3');
       break;
 
     default:
